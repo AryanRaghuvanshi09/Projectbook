8ac2697c4481d407b5b0851c431ee4195239c818
Aryan Raghuvanshi <124778245+AryanRaghuvanshi09@users.noreply.github.com>
2024-12-04 20:02:03 +0530

    Update api.js

--- backend/frontend/src/services/api.js
+++ backend/frontend/src/services/api.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+// Determine the base URL based on the environment
+const baseURL = process.env.NODE_ENV === 'production'
+    ? 'https://projectbook.onrender.com/api'  // Live URL for production
+    : 'http://localhost:5000/api';  // Local URL for development
+
+const API = axios.create({ baseURL });
 
 // Interceptor to attach the token to requests if logged in
 API.interceptors.request.use((req) => {
